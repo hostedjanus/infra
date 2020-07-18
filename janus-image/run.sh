df5819490c663d15f4667794920f9ce3dcaec9a6
Cristian Paul Penaranda Rojas <paul@kristianpaul.org>
2020-07-17 23:01:14 -0500

    fix: minimal changes to run janus

--- janus-image/run.sh
+++ janus-image/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-
-/opt/janus/bin/janus
+#Enable admin api
+sed -i "s/admin_http = false/admin_http = true/g" /opt/janus/etc/janus/janus.transport.http.jcfg
+curl ifconfig.co/ip -s > publicip
+myip=\$(cat publicip)
+sed -i "s/#stun_server/stun_server/g" /opt/janus/etc/janus/janus.jcfg
+sed -i "s/#stun_port/stun_port/g" /opt/janus/etc/janus/janus.jcfg
+sed -i "s/#rtp_port_range/rtp_port_range/g" /opt/janus/etc/janus/janus.jcfg
+sed -i "s/#nat_1_1_mapping = \"1.2.3.4\"/nat_1_1_mapping = \"$myip\"/g" /opt/janus/etc/janus/janus.jcfg
